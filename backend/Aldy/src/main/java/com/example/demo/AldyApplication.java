58bb7a53458fe15b1a31f32c1f7db2f7fa4122cd
조성민 <chob5864@gmail.com>
2022-10-18 11:00:23 +0900

    initial commit

---
+++ backend/Aldy/src/main/java/com/example/demo/AldyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AldyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AldyApplication.class, args);
+	}
+
+}
