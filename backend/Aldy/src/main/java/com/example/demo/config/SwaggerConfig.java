dfa546bc5cab6638f32bde26708fa20f2e7479bd
Seokho Hong <seokho.hong.dev@gmail.com>
2022-10-19 17:44:15 +0900

    [BE] feat : Swagger 적용

---
+++ backend/Aldy/src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                        .title("ALDY")
+                        .description("ALDY's Swagger")
+                        .version("v0.0.1"));
+
+    }
+
+}
