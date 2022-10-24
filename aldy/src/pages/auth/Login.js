fb58377e06be0e12cab8133cee8ed764016268c6
문서희 <shhhm@hanmail.net>
2022-10-24 16:36:57 +0900

    [FE] style : 로그인 페이지 틀 생성

--- aldy/src/pages/auth/Login.js
+++ aldy/src/pages/auth/Login.js
@@ -1,5 +1,28 @@
 const Login = () => {
-  return <div>로그인</div>;
+  return (
+    <div>
+      <section>
+        <div>로그인을 진행해주세요.</div>
+        <div>로그인</div>
+      </section>
+      <section>
+        <div>
+          <div>아이디</div>
+          <form>
+            <input></input>
+          </form>
+        </div>
+        <div>
+          <div>비밀번호</div>
+          <input></input>
+        </div>
+        <div>
+          <button>회원가입</button>
+          <button>로그인</button>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Login;
