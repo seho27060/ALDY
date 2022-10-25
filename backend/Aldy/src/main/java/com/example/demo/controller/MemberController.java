dc037db4eb93a4603d7fa75c8f57c633ca573b30
Seho <seho27060@gmail.com>
2022-10-25 10:34:30 +0900

    merge commit

--- backend/Aldy/src/main/java/com/example/demo/controller/MemberController.java
+++ backend/Aldy/src/main/java/com/example/demo/controller/MemberController.java
@@ -1,5 +1,15 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
 public class MemberController {
+
+    @PostMapping("/join")
+    public String
 
 }
