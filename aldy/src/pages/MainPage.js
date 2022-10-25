239a0d3bdf69f64923bad8bf99f3d565f86b29ce
문서희 <shhhm@hanmail.net>
2022-10-25 13:39:23 +0900

    [FE] style : 메인 페이지 버튼  Styled Component 적용

--- aldy/src/pages/MainPage.js
+++ aldy/src/pages/MainPage.js
@@ -1,3 +1,27 @@
+import styled from "styled-components";
+
+const RedButton = styled.button`
+  width: 170px;
+  border-radius: 8px;
+  background-color: red;
+  border: none;
+  outline: none;
+  color: white;
+  font-weight: bold;
+  transition: transform 30ms ease-in;
+`;
+
+const WhiteButton = styled.button`
+  width: 170px;
+  border-radius: 8px;
+  background-color: white;
+  border: none;
+  outline: none;
+  color: red;
+  font-weight: bold;
+  transition: transform 30ms ease-in;
+`;
+
 const MainPage = () => {
   return (
     <main>
@@ -9,7 +33,7 @@ const MainPage = () => {
           <div>✨ 스터디 레벨에 따라 성장하는 공룡 ✨</div>
           <div>스터디원들과 함께 공룡을 키워봐요!</div>
         </div>
-        <button>스터디 생성하기</button>
+        <RedButton>스터디 생성하기</RedButton>
       </section>
       <section>
         <div>ALDY만의 코드리뷰 이용하기</div>
@@ -18,7 +42,7 @@ const MainPage = () => {
             <span>문제풀이 과정을</span>
             <span>4단계로 세분화!</span>
           </p>
-          <button>회원가입 하러가기</button>
+          <WhiteButton>회원가입 하러가기</WhiteButton>
         </div>
       </section>
     </main>
