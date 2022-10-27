6f5264dfe1c799f7d3ff38a0d52c140e7bb339ac
문서희 <shhhm@hanmail.net>
2022-10-27 15:28:41 +0900

    [FE] style : 회원가입페이지 오른쪽 section 퍼블리싱

--- aldy/src/pages/auth/Signup.js
+++ aldy/src/pages/auth/Signup.js
@@ -15,27 +15,34 @@ const RedButton = styled.button`
 const Signup = () => {
   return (
     <main className="signup-page-main">
-      <section>
-        <div>Sign Up</div>
-        <div>
-          <div>아이디</div>
-          <input></input>
-          <RedButton>인증하기</RedButton>
-        </div>
-        <div>
-          <div>비밀번호</div>
-          <input></input>
-        </div>
-        <div>
-          <div>비밀번호 확인</div>
-          <input></input>
-        </div>
-      </section>
-      <section>
-        <div>Welcome to Aldy</div>
-        <div>Aldy와 함께 알고리즘 스터디를 키워보세요!</div>
-        <img src={process.env.PUBLIC_URL + "/signup_dinosaur.png"} alt=""></img>
-      </section>
+      <div className="signup-page-bg">
+        <section>
+          <div className="left-title">Sign Up</div>
+          <div>
+            <div>아이디</div>
+            <input></input>
+            <RedButton>인증하기</RedButton>
+          </div>
+          <div>
+            <div>비밀번호</div>
+            <input></input>
+          </div>
+          <div>
+            <div>비밀번호 확인</div>
+            <input></input>
+          </div>
+        </section>
+        <section className="signup-page-right">
+          <div className="signup-page-right-title">✨Welcome to Aldy✨</div>
+          <div className="signup-page-right-text">
+            Aldy와 함께 알고리즘 스터디를 키워보세요!
+          </div>
+          <img
+            src={process.env.PUBLIC_URL + "/signup_dinosaur.png"}
+            alt=""
+          ></img>
+        </section>
+      </div>
     </main>
   );
 };
