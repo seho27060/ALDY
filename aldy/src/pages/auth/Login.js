e66139b9174c85b41ae56ce4d51b7f89e7c14bb2
문서희 <shhhm@hanmail.net>
2022-10-28 09:23:59 +0900

    [FE] style : 로그인페이지 이미지 삽입

--- aldy/src/pages/auth/Login.js
+++ aldy/src/pages/auth/Login.js
@@ -1,27 +1,56 @@
+import "./Login.css";
+import styled from "styled-components";
+
+const RedButton = styled.button`
+  width: 170px;
+  border-radius: 8px;
+  background-color: red;
+  border: none;
+  outline: none;
+  color: white;
+  font-weight: bold;
+  transition: transform 30ms ease-in;
+`;
+
 const Login = () => {
   return (
-    <div>
-      <section>
-        <div>로그인을 진행해주세요.</div>
-        <div>로그인</div>
-      </section>
-      <section>
-        <div>
-          <div>아이디</div>
+    <main className="login-page-main">
+      <div className="login-page-bg">
+        <section className="login-page-left">
+          <div className="nnnnnn">Login</div>
           <form>
-            <input></input>
+            <div className="form-title">
+              <div>아이디</div>
+              <div className="form-title-id">
+                <input placeholder="아이디를 입력해주세요."></input>
+              </div>
+            </div>
+            <div className="form-title">
+              <div>비밀번호</div>
+              <input placeholder="비밀번호를 입력해주세요."></input>
+            </div>
+            <div className="login-submit-btn">
+              <RedButton>Log In</RedButton>
+            </div>
           </form>
-        </div>
-        <div>
-          <div>비밀번호</div>
-          <input></input>
-        </div>
-        <div>
-          <button>회원가입</button>
-          <button>로그인</button>
-        </div>
-      </section>
-    </div>
+          <div>
+            <p>아직 계정이 없으신가요? </p>
+            <p> 회원가입 하러 가기</p>
+          </div>
+        </section>
+        <section className="login-page-right">
+          <div className="login-page-right-title">✨Welcome to Aldy✨</div>
+          <div className="login-page-right-text">
+            if (not a member?)
+            {<button>JOIN US</button>}
+          </div>
+          <img
+            src={process.env.PUBLIC_URL + "/login_dinosaur.png"}
+            alt=""
+          ></img>
+        </section>
+      </div>
+    </main>
   );
 };
 
