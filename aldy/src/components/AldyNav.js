1568b7f9cb7690ea26c7a96d06553eb5f1520ae8
세룽룽 <zmmmm111@gmail.com>
2022-10-28 10:58:45 +0900

    [FE] style: 네비게이션

---
+++ aldy/src/components/AldyNav.js
@@ -0,0 +1,67 @@
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Offcanvas from "react-bootstrap/Offcanvas";
+
+import "./AldyNav.css";
+import styled from "styled-components";
+
+const RedButton = styled.button`
+  width: 170px;
+  border-radius: 8px;
+  background-color: red;
+  border: none;
+  outline: none;
+  color: white;
+  font-weight: bold;
+  transition: transform 30ms ease-in;
+`;
+
+const AldyNav = () => {
+  return (
+    <Navbar expand="md">
+      <Container fluid>
+        <Navbar.Brand href="#" className="nav-title">
+          Aldy
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="offcanvasNavbar-expand-md" />
+        <Navbar.Offcanvas
+          id="offcanvasNavbar-expand-md"
+          aria-labelledby="offcanvasNavbarLabel-expand-md"
+          placement="end"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id="offcanvasNavbarLabel-expand-md">
+              Offcanvas
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action2">Link</Nav.Link>
+              <NavDropdown
+                title="Dropdown"
+                id="offcanvasNavbarDropdown-expand-md"
+              >
+                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">
+                  Something else here
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <RedButton>로그인</RedButton>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default AldyNav;
--- aldy/src/components/Nav.js
+++
@@ -1,9 +0,0 @@
-const Nav = () => {
-    return (
-        <div>
-            Navbar입니다.
-        </div>
-    )
-}
-
-export default Nav;
