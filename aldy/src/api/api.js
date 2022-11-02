c22720ec51e41c6dacccfaeb5ec3f513b20c19e6
joey0319 <joey0319@naver.com>
2022-11-02 14:26:53 +0900

    [FE] feat : axios interceptor 설정

--- aldy/src/api/api.js
+++ aldy/src/api/api.js
@@ -1,20 +1,14 @@
 import axios from "axios";
 
-export const baseURL = process.env.REACT_APP_API_URL + "/api";
+const baseURL = process.env.REACT_APP_API_URL + "/api";
 
 export const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL + "/api",
-  headers: {
-    Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
-    "Content-Type": "application/json",
-  },
+  baseURL: baseURL,
 });
 
 // api 요청 인터셉터
 api.interceptors.request.use((config) => {
-  config.headers.Authorization = `Bearer ${sessionStorage.getItem(
-    "accessToken"
-  )}`;
+  config.headers.Authorization = `Bearer ${sessionStorage.getItem("accessToken")}`;
   return config;
 });
 
@@ -27,30 +21,22 @@ api.interceptors.response.use(
 
   // 실패 응답일 때,
   async (error) => {
-    // A) 토큰 만료 이슈인 경우
-    if (error.response.data?.message.includes("만료")) {
+    // 1) 토큰 만료 이슈인 경우
+    if (error.response.status === 401 && error.response.data.code === 'ACCESSTOKEN_EXPIRED') {
       // a) 갱신 요청
-      const { data, status } = await axios.post(baseURL + `/member/refresh`, {
-        headers: {
-          Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
-        },
-      });
-      if (status === 201) {
-        localStorage.setItem("accessToken", data.accessToken);
+      const { data, status } = await axios.post(baseURL + '/member/refresh',
+      {refreshToken : sessionStorage.getItem('refreshToken')});
+      if (status === 200) {
+        sessionStorage.setItem("accessToken", data.accessToken);
+        console.log('토큰이 갱신되었습니다.') // 나중에 지울것
 
-        // 헤더 변경 후 다시 쏘기
+        // 원래 요청에서 토큰 변경 후 다시 요청하기
         const originalRequest = error.config;
-        axios.defaults.headers.common.Authorization = `Bearer ${sessionStorage.getItem(
-          "accessToken"
-        )}`;
-        originalRequest.headers.Authorization = `Bearer ${localStorage.getItem(
-          "accessToken"
-        )}`;
-
-        return await axios(originalRequest);
+        originalRequest.headers.Authorization = `Bearer ${sessionStorage.getItem("accessToken")}`;
+        return await api.request(originalRequest)
       }
     }
-    // B) 토큰 이슈 아닌 경우 및 refreshToken 만료 이슈
+    // 2) 토큰 이슈 아닌 경우 및 refreshToken 만료 이슈
     return Promise.reject(error);
   }
 );
