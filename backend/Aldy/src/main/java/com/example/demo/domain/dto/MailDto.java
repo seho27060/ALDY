27270ce58e9d1988ec3ac8342f3ca34f88e6394b
조성민 <chob5864@gmail.com>
2022-11-02 09:55:50 +0900

    [BE] feat : 이메일 API 추가 완료

--- backend/Aldy/src/main/java/com/example/demo/domain/dto/MailDto.java
+++ backend/Aldy/src/main/java/com/example/demo/domain/dto/MailDto.java
@@ -1,16 +1,18 @@
 package com.example.demo.domain.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
+@Builder
 public class MailDto {
     private String address;
 //    request, reply, evict
     private String type;
-    private String nickname;
+    private String receiver_nickname;
+    private String sender_nickname;
     private String StudyName;
 }
