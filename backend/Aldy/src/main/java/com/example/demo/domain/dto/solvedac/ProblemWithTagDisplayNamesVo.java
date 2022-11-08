eba860a5ec1059a3864d03982d5ffb85408075c8
Seho <seho27060@gmail.com>
2022-11-08 14:27:02 +0900

    [BE] fix : Member mypage API 반환값 수정 1. member db id 도 반환함

--- backend/Aldy/src/main/java/com/example/demo/domain/dto/solvedac/ProblemWithTagDisplayNamesVo.java
+++ backend/Aldy/src/main/java/com/example/demo/domain/dto/solvedac/ProblemWithTagDisplayNamesVo.java
@@ -5,10 +5,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+import java.util.List;
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @SuperBuilder
 public class ProblemWithTagDisplayNamesVo extends ProblemVo{
-
+    private List<TagsWithDisplayNamesDto> tagsWithDisplayNamesDtoList;
 }
