fd3db2d397da06d6a6a43ccfc5b90c7f2b93b619
seohmoon <dream222ing@gmail.com>
2022-11-12 19:20:03 +0900

    [FE] style : 스터디 가입신청 모달 alert창 Modal로 변경

--- aldy/src/components/study/StudyJoinModal.js
+++ aldy/src/components/study/StudyJoinModal.js
@@ -4,6 +4,8 @@ import styled from "styled-components";
 import { useState, useEffect, useRef } from "react";
 import { studyRegister } from "../../api/study";
 import { useNavigate } from "react-router-dom";
+import AlertModal from "../../components/AlertModal";
+import AlertRefreshModal from "../../components/AlertRefreshModal";
 
 const RedButton = styled.button`
   width: 170px;
@@ -22,27 +24,49 @@ const StudyJoin = ({ studyDetail, modal, handleModal }) => {
   const navigateStudy = () => {
     navigate("/study/list");
   };
-  console.log(studyDetail, "안녕하세요, 저는 모달창입니다.");
+  // console.log(studyDetail, "안녕하세요, 저는 모달창입니다.");
   const messageInput = useRef(null);
   const [sendJoin, setSendJoin] = useState({
     studyId: studyDetail.id,
     message: "",
   });
+
+  const [message, setMessage] = useState("");
+  const [alertModalShow, setAlertModalShow] = useState(false);
+  const [alertRefreshModalShow, setAlertRefreshModalShow] = useState(false);
+
   const onSubmit = () => {
     setSendJoin((sendJoin.message = messageInput.current.value));
     studyRegister(sendJoin)
       .then((res) => {
-        alert("가입 신청이 완료되었습니다.");
-        navigateStudy();
+        // alert("가입 신청이 완료되었습니다.");
+        // navigateStudy();
+        setMessage("가입 신청이 완료되었습니다.");
+        setAlertModalShow(true);
       })
       .catch((err) => {
-        alert("가입 신청을 다시 진행해주세요.");
+        // alert("가입 신청을 다시 진행해주세요.");
+        setMessage("가입 신청을 다시 진행해주세요.");
+        setAlertRefreshModalShow(true);
       });
     // console.log(messageInput.current.value, "제출");
     // console.log(studyDetail.id, "I'm study id number.");
   };
   return (
     <Modal size="lg" show={modal} onHide={handleModal}>
+      <AlertModal
+        show={alertModalShow}
+        onHide={() => {
+          setAlertModalShow(false);
+          navigateStudy();
+        }}
+        message={message}
+      />
+      <AlertRefreshModal
+        show={alertRefreshModalShow}
+        onHide={() => setAlertRefreshModalShow(false)}
+        message={message}
+      />
       <Modal.Body className="review-modal-body">
         <div className="review-modal-header">
           <div>
