14cf2df24f9981f3e60e3b3da7409127ec856799
joey0319 <joey0319@naver.com>
2022-11-17 16:28:05 +0900

    [FE] feat : 메인페이지 이동 탭 기능 구현 완료

---
+++ aldy/src/components/MobeBar.js
@@ -0,0 +1,36 @@
+import './MoveBar.css'
+
+const MoveBar = () => {
+    const moveService = () => {
+        // 서비스 설명 위치로 이동
+        const top = document.querySelector('.main-page-description').offsetTop;
+        window.scrollTo({top:top, behavior:'smooth'})
+    }
+    const moveCharacter = () => {
+        // 캐릭터 설명 위치로 이동
+        const top = document.querySelector('.aldy_step_info').offsetTop;
+        window.scrollTo({top:top, behavior:'smooth'})
+    }
+
+    const moveCodeReview = () => {
+        // 코드리뷰 설명 위치로 이동
+        const top = document.querySelector('#main-page-description-aldylist').offsetTop;
+        window.scrollTo({top:top, behavior:'smooth'})
+    }
+
+    const moveStudy = () => {
+        // 스터디 설명 위치로 이동
+        const top = document.querySelector('.main-page-study-description').offsetTop;
+        window.scrollTo({top:top, behavior:'smooth'})
+    }
+    return (
+        <div className='move-bar'>
+            <button className='move-bar-btn' onClick={moveService}>서비스 설명</button>
+            <button className='move-bar-btn' onClick={moveCharacter}>캐릭터 설명</button>
+            <button className='move-bar-btn' onClick={moveCodeReview}>코드리뷰 설명</button>
+            <button className='move-bar-btn' onClick={moveStudy}>스터디 설명</button>
+        </div>
+    )
+}
+
+export default MoveBar;
